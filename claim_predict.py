e08e9de863f62fd6adc3682ece33e9fe19e567c2
srideepika <j.srideepika@gmail.com>
2018-01-28 05:35:00 -0500

    prediction with results for graph

--- claim_predict.py
+++ claim_predict.py
@@ -13,6 +13,7 @@ Y_test=[]
 with open('/media/d/9485fdcc-4df8-4558-a82b-390eaf04ac40/d/ben_drug_claims.csv', 'r') as csvfile:
      reader = csv.reader(csvfile)
      i=0
+     
      min=float(sys.maxsize)
      max=-min
      for row in reader:
@@ -20,58 +21,37 @@ with open('/media/d/9485fdcc-4df8-4558-a82b-390eaf04ac40/d/ben_drug_claims.csv',
             print(row)
             i=1
             continue
-        
-         if(row[-1]=='2008'):
-                 X.append([float(row[-6]),float(row[-5]),float(row[-4]),float(row[-7])])
-                 if(float(row[-2])<min):
+         
+         X.append([float(row[-6]),float(row[-5]),float(row[-3]),float(row[-7])])
+         if(float(row[-2])<min):
                     min=float(row[-2])
-                 if(float(row[-2])>max):
+         if(float(row[-2])>max):
                     max=float(row[-2])
-                 Y.append(float(row[-2]))
-         elif(row[-1]=='2010'):
-                 X_test.append([float(row[-6]),float(row[-5]),float(row[-4]),float(row[-7])])
-                 Y_test.append(float(row[-2]))
-         #except:
-          #  print(row)
-           # pass
-#print(df.columns)
-#print('Xz')
-#Y = df['CLM_PMT_AMT']
-#X = df[['NCH_BENE_BLOOD_DDCTBL_LBLTY_AM'],'PRVDR_NUM']
-#X.append(df['PRVDR_NUM'])
+                    
+         Y.append(float(row[-2]))
+         i+=1
+         
 print(X[:10])
 print(len(X),len(Y))
 print('maxmin',max,min)
-#X=X.reshape(len(X),1)
-#Y=Y.reshape(len(Y),1)
- 
-# Split the data into training/testing sets
-#X_train = X[:-550]
-#X_test = X[:-550]
- 
-# Split the targets into training/testing sets
-#Y_train = Y[:-550]
-#Y_test = Y[:-550]
- 
- 
-# Plot outputs
-#plt.scatter(X_test, Y_test,  color='black')
-#plt.title('Test Data')
-#plt.xlabel('NCE_BENE')
-#plt.ylabel('Price')
-#plt.xticks(())
-#plt.yticks(())
- 
-#plt.show()
 
 regr = linear_model.LinearRegression(normalize=True,fit_intercept=True)
  
 # Train the model using the training sets
-regr.fit(X, Y)
+regr.fit(X[:-500], Y[:-500])
  
 # Plot outputs
 #plt.plot(X_test, regr.predict(X_test), color='red',linewidth=3)
-p=regr.predict(X_test[:150])
-plt.plot(p,Y_test[:150])
-plt.show()
-print(sum(abs(Y_test[:150]-p))/len(p))
+p=regr.predict(X[-500:])
+
+print(sum(abs(Y[-500:]-p))/len(p))
+#Max 63000.0 Min 0 MAE=1043.334
+'''
+['', 'DESYNPUF_ID', 'BENE_BIRTH_DT', 'BENE_DEATH_DT', 'BENE_SEX_IDENT_CD', 'BENE_RACE_CD', 'BENE_ESRD_IND', 'SP_STATE_CODE', 'BENE_COUNTY_CD', 'BENE_HI_CVRAGE_TOT_MONS', 'BENE_SMI_CVRAGE_TOT_MONS', 'BENE_HMO_CVRAGE_TOT_MONS', 'PLAN_CVRG_MOS_NUM', 'SP_ALZHDMTA', 'SP_CHF', 'SP_CHRNKIDN', 'SP_CNCR', 'SP_COPD', 'SP_DEPRESSN', 'SP_DIABETES', 'SP_ISCHMCHT', 'SP_OSTEOPRS', 'SP_RA_OA', 'SP_STRKETIA', 'MEDREIMB_IP', 'BENRES_IP', 'PPPYMT_IP', 'MEDREIMB_OP', 'BENRES_OP', 'PPPYMT_OP', 'MEDREIMB_CAR', 'BENRES_CAR', 'PPPYMT_CAR', 'PDE_ID', 'SRVC_DT', 'PROD_SRVC_ID', 'QTY_DSPNSD_NUM', 'DAYS_SUPLY_NUM', 'PTNT_PAY_AMT', 'TOT_RX_CST_AMT', 'NCH_BENE_DSCHRG_DT', 'SUM_CLM_AMT', 'year']
+Predicted:
+[ 9146.59987089] 
+Test vals:
+Male, with Heart failure, renal failure, diabetes, alzheimers, osteoporasis, with drug 490713828, in the year 2009, has predicted $9146 when he actually spent $8000.
+8000.0 ['873815', 'FFE7C75AE1426A4D', '19400901', '', '1', '1', '0', '33', '320', '12', '12', '12', '12', '1', '1', '2', '2', '2', '1', '1', '1', '1', '2', '2', '0.0', '0.0', '0.0', '0.0', '0.0', '0.0', '2660.0', '580.0', '0.0', '233734490227038', '20080215', '490713828', '0.0', '30', '0.0', '400.0', '20090726', '8000.0', '2009']
+'''
+
